f572042266181d1d7e296468db09fd3c24b7ab5d
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-08-29 18:52:36 +0100

    :fire: NEW - READY TO DEPLOY

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,6 @@
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -112,3 +112,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+django_heroku.settings(locals())
